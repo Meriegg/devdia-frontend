92eab2720772c2df9eb33aa847eeb92ce82441a5
Meriegg <ya.boybeats1234@gmail.com>
2022-06-22 23:52:12 +0300

    Added apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
 import Layout from "../Components/Layout";
 import MantineLibProvider from "../Components/MantineLibProvider";
+import gqlclient from "../graphql/gqlclient";
+import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineLibProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </MantineLibProvider>
+    <ApolloProvider client={gqlclient}>
+      <MantineLibProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MantineLibProvider>
+    </ApolloProvider>
   );
 }
 
